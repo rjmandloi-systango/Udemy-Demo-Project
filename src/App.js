6353fade81a071991cdc4dfbdd550f8129ca1e9f
rjmandloi-systango <rjmandloi@isystango.com>
2022-07-06 15:05:21 +0530

    Feat:React fragment intoduction and usage in app.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import React , {useState} from 'react';
+import React, { useState } from 'react';
 import UserCard from './components/Users/UserCard'
 import './App.css';
 import AddUser from './components/Users/AddUserForm';
-import Wrapper from './components/wrapper/Wrapper';
 
 
 function App() {
- const [usersList , setUsersList] = useState([])
- const addUserHandler=(userName , userAge)=>{
-  setUsersList((prevUsersList)=>{return [...prevUsersList ,{id:Math.random() ,name:userName ,age:userAge}]})
- }
+  const [usersList, setUsersList] = useState([])
+  const addUserHandler = (userName, userAge) => {
+    setUsersList((prevUsersList) => { return [...prevUsersList, { id: Math.random(), name: userName, age: userAge }] })
+  }
   return (
-    <Wrapper>
-     {/* <div className="App"> */}
-      <AddUser  onAddUser={addUserHandler}/>
+    <React.Fragment>
+      {/* <Wrapper> */}
+      {/* <div className="App"> */}
+      <AddUser onAddUser={addUserHandler} />
       <div>
-        <UserCard  users={usersList}/>
+        <UserCard users={usersList} />
       </div>
-      
-     {/* </div> */}
-    </Wrapper>
+
+      {/* </div> */}
+      {/* </Wrapper> */}
+    </React.Fragment>
   );
 }
 
